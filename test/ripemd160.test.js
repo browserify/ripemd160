b907b680523f863442e4beb7a39765dbeff9d687
Brooks Boyd <brooks.boyd@forteresearch.com>
2013-12-10 08:11:34 -0600

    Node-specific implementation
    Use the native Crypto object if running on a Node.js platform

--- test/ripemd160.test.js
+++ test/ripemd160.test.js
@@ -7,7 +7,19 @@ describe('+ ripemd160(input)', function() {
     it('should compute the ripemd160 hash', function() {
       var input = "hello"
       EQ (ripemd160(input), "108f07b8382412612c048d07d13f814118445acd")
-    })
+    }),
+	it('should return an array when asked', function() {
+		var test = ripemd160('hello', { asBytes:true });
+		T (Array.isArray(test));
+		EQ (test.join(','), '16,143,7,184,56,36,18,97,44,4,141,7,209,63,129,65,24,68,90,205');
+	}),
+	it('should return a binary string when asked', function() {
+		var test = ripemd160('hello', { asString:true });
+		var chars = [16,143,7,184,56,36,18,97,44,4,141,7,209,63,129,65,24,68,90,205];
+		EQ (test, chars.reduce(function(prev, cur, index, arr) {
+			return prev+String.fromCharCode(cur);
+		}, ''));
+	})
   })
 })
 
