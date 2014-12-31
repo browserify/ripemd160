50bd612b7ff2b75082108388acd43b42d2463ee7
JP Richardson <jprichardson@gmail.com>
2014-12-31 05:06:19 -0600

    ripemd160: removed terst for assert

--- test/ripemd160.test.js
+++ test/ripemd160.test.js
@@ -1,3 +1,4 @@
+var assert = require('assert')
 var ripemd160 = require('../lib/ripemd160')
 
 require('terst')
@@ -6,14 +7,14 @@ describe('+ ripemd160(input)', function() {
   describe('> when input is a string', function() {
     it('should compute the ripemd160 hash', function() {
       var input = "hello";
-      EQ (ripemd160(input).toString('hex'), "108f07b8382412612c048d07d13f814118445acd")
+      assert.equal(ripemd160(input).toString('hex'), "108f07b8382412612c048d07d13f814118445acd")
     })
   })
 
   describe('> when input is a buffer', function() {
     it('should compute the ripemd160 hash', function() {
       var input = new Buffer("hello");
-      EQ (ripemd160(input).toString('hex'), "108f07b8382412612c048d07d13f814118445acd")
+      assert.equal(ripemd160(input).toString('hex'), "108f07b8382412612c048d07d13f814118445acd")
     })
   })
 })
