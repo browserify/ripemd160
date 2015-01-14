dbea352b68748dde15afa4289bb887dd66ae6742
JP Richardson <jprichardson@gmail.com>
2015-01-14 06:47:00 -0600

    test/ripemd160: verified array input would work

--- test/ripemd160.test.js
+++ test/ripemd160.test.js
@@ -6,21 +6,32 @@ var fixtures = require('./fixtures')
 
 describe('ripemd160(input)', function() {
   fixtures.valid.forEach(function(f) {
-    if (f.encoding === 'utf8') {
-      describe('> when input (' + f.data + ') encoding is utf8', function() {
-        it('should accept a native string and produce a result', function() {
-          var result = ripemd160(f.data)
+    if (typeof f.data == 'string') {
+      if (f.encoding === 'utf8') {
+        describe('> when input (' + f.data + ') encoding is utf8', function() {
+          it('should accept a native string and produce a result', function() {
+            var result = ripemd160(f.data)
+            assert.equal(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
+          })
+        })
+      }
+
+      describe('> when input ('+ (new Buffer(f.data, f.encoding)).toString('hex') + ')is any encoding', function() {
+        it('should accept a buffer and produce a result', function() {
+          var result = ripemd160(new Buffer(f.data, f.encoding))
           assert.equal(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
         })
       })
     }
 
-    describe('> when input ('+ (new Buffer(f.data, f.encoding)).toString('hex') + ' )is any encoding', function() {
-      it('should accept a buffer and produce a result', function() {
-        var result = ripemd160(new Buffer(f.data, f.encoding))
-        assert.equal(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
+    if (Array.isArray(f.data)) {
+      describe('> when input ('+ (new Buffer(f.data)).toString('hex') + ') is an array', function() {
+        it('should accept a buffer and produce a result', function() {
+          var result = ripemd160(f.data)
+          assert.equal(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
+        })
       })
-    })
+    }
   })
 })
 
