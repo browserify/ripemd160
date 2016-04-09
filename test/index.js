1b639520a32ddca32f5a37b7ccdbe78581531dc6
Kirill Fomichev <fanatid@ya.ru>
2016-04-09 12:51:19 +0300

    add streams support, unroll loops

--- test/index.js
+++ test/index.js
@@ -1,47 +1,21 @@
-var tap = require('tap')
-var ripemd160 = require('../')
+'use strict'
+var test = require('tape').test
+var vectors = require('hash-test-vectors')
 
-// from http://homes.esat.kuleuven.be/~bosselae/ripemd160.html
-var fixtures = require('./fixtures')
+var RIPEMD160 = require('../')
 
-tap.test('ripemd160(input)', function (t) {
-  fixtures.valid.forEach(function (f) {
-    if (typeof f.data === 'string') {
-      if (f.encoding === 'utf8') {
-        t.test('> when input (' + f.data + ') encoding is utf8', function (t) {
-          t.test('should accept a native string and produce a result', function (t) {
-            var result = ripemd160(f.data)
-            t.same(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
-            t.end()
-          })
+vectors.forEach(function (vector, i) {
+  var input = new Buffer(vector.input, 'base64')
 
-          t.end()
-        })
-      }
-
-      t.test('> when input (' + (new Buffer(f.data, f.encoding)).toString('hex') + ')is any encoding', function (t) {
-        t.test('should accept a buffer and produce a result', function (t) {
-          var result = ripemd160(new Buffer(f.data, f.encoding))
-          t.same(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
-          t.end()
-        })
-
-        t.end()
-      })
-    }
-
-    if (Array.isArray(f.data)) {
-      t.test('> when input (' + (new Buffer(f.data)).toString('hex') + ') is an array', function (t) {
-        t.test('should accept a buffer and produce a result', function (t) {
-          var result = ripemd160(f.data)
-          t.same(result.toString('hex'), (new Buffer(f.result, 'hex')).toString('hex'))
-          t.end()
-        })
-
-        t.end()
-      })
-    }
+  test('vector #' + (i + 1) + ' with .update', function (t) {
+    t.same(new RIPEMD160().update(input).digest('hex'), vector.ripemd160)
+    t.end()
   })
 
-  t.end()
+  test('vector #' + (i + 1) + ' with streams', function (t) {
+    var hash = new RIPEMD160()
+    hash.end(input)
+    t.same(hash.read().toString('hex'), vector.ripemd160)
+    t.end()
+  })
 })
